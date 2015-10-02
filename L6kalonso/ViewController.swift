d11e1b3958b43d8de8d783dfa6df1762d37b62ab
Kelly Alonso-Palt <kelly.alonso@gmail.com>
2015-10-02 16:10:44 -0700

    Initial Commit

---
+++ L6kalonso/ViewController.swift
@@ -0,0 +1,210 @@
+//
+//  ViewController.swift
+//  L6kalonso
+//
+//  Created by Kelly Alonso-Palt on 9/21/15.
+//  Copyright © 2015 Kelly Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //MARK: Properties
+    
+    @IBOutlet var easyLabel: UILabel!
+    
+    @IBOutlet var mediumLabel: UILabel!
+    
+    @IBOutlet var hardLabel: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
+    var theLabels = [UILabel]()
+//    var theTimer = NSTimer()
+//   
+//    var seconds: NSTimeInterval = 60
+    
+    
+    var easyOne = 0
+    var easyTwo = 0
+    var easyAnswer = 0
+    var medOne = 0
+    var medTwo = 0
+    var medAnswer = 0
+    var hardOne = 0
+    var hardTwo = 0
+    var hardAnswer = 0
+    
+    var possibleAnswer: Int? = 0
+    var currentAnswer = 0
+    
+    var userScore = 0
+    var currentGoal = 20
+    
+    var easyRange: UInt32 = 10
+    var mediumRange: UInt32 = 20
+    var hardRange: UInt32 = 50
+    
+    
+    
+    //MARK: Initialization
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTheGame()
+//        theTimer = setUpTheTimer()
+//        startTheTimer(theTimer)
+    }
+    
+    func startTheGame(alert: UIAlertAction! = nil) {
+        
+        if userScore >= currentGoal {
+            easyRange *= 2
+            mediumRange *= 2
+            hardRange *= 2
+            currentGoal += 20
+        }
+        
+        generateNewLabels(easyRange, medium: mediumRange, hard: hardRange)
+        
+            textField.delegate = self
+            textField.placeholder = "Type the answer to any of the three questions"
+            textField.becomeFirstResponder()
+    }
+    
+    func generateNewLabels(easy: UInt32, medium: UInt32, hard: UInt32) {
+      
+        
+        easyOne = Int(arc4random_uniform(easy))
+        easyTwo = Int(arc4random_uniform(easy))
+        easyAnswer = easyOne + easyTwo
+        
+        easyLabel.text = "\(easyOne) + \(easyTwo)"
+        
+        medOne = Int(arc4random_uniform(medium))
+        medTwo = Int(arc4random_uniform(medium))
+        medAnswer = medOne + medTwo
+        
+        mediumLabel.text = "\(medOne) + \(medTwo)"
+        
+        hardOne = Int(arc4random_uniform(hard))
+        hardTwo = Int(arc4random_uniform(hard))
+        hardAnswer = hardOne + hardTwo
+        
+        hardLabel.text = "\(hardOne) + \(hardTwo)"
+
+    }
+    
+//    func setUpTheTimer() -> NSTimer {
+//        var newTimer = NSTimer(timeInterval: seconds, invocation: viewDidLoad(), repeats: false)
+//        return newTimer
+//    }
+//    
+//    func startTheTimer(theTimer: NSTimer) {
+//       
+//        theTimer.fire()
+//        
+//    }
+
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        if let possibleAnswer = Int(textField.text!) {
+//            textField.placeholder = "Touch the corresponding square to assign"
+//            textField.text = ""
+            currentAnswer = possibleAnswer
+        }
+        else {
+            
+            startTheGame()
+        }
+    }
+    
+
+
+   //MARK: Actions
+    
+    @IBAction func easyButton(sender: UIButton) {
+        
+        easyLabel.text = "\(easyOne) + \(easyTwo) = \(currentAnswer)"
+        textField.placeholder = "Touch the corresponding square to assign"
+        textField.text = ""
+
+        if currentAnswer == easyAnswer {
+            
+            let ac = UIAlertController(title: title, message: "Correct! Your score is \(++userScore).", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: startTheGame))
+            presentViewController(ac, animated: true, completion: nil)
+
+        }
+            
+        else {
+            
+            let ac = UIAlertController(title: title, message: "Incorrect", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: startTheGame))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+
+    
+    @IBAction func mediumButton(sender: UIButton) {
+        
+        textField.placeholder = "Touch the corresponding square to assign"
+        textField.text = ""
+
+        mediumLabel.text = "\(medOne) + \(medTwo) = \(currentAnswer)"
+        
+        if currentAnswer == medAnswer {
+            
+            userScore += 2
+            
+            let ac = UIAlertController(title: title, message: "Correct! Your score is \(userScore).", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: startTheGame))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+            
+        else {
+            
+            let ac = UIAlertController(title: title, message: "Incorrect", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: startTheGame))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func hardButton(sender: UIButton) {
+        
+        textField.placeholder = "Touch the corresponding square to assign"
+        textField.text = ""
+
+        
+        hardLabel.text = "\(hardOne) + \(hardTwo) = \(currentAnswer)"
+        
+        if currentAnswer == hardAnswer {
+            
+            userScore += 4
+            
+            let ac = UIAlertController(title: title, message: "Correct! Your score is \(userScore).", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: startTheGame))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+            
+        else {
+            
+            let ac = UIAlertController(title: title, message: "Incorrect", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: startTheGame))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+}
+
---
+++ L6kalonsoTests/L6kalonsoTests.swift
@@ -0,0 +1,36 @@
+//
+//  L6kalonsoTests.swift
+//  L6kalonsoTests
+//
+//  Created by Kelly Alonso-Palt on 9/21/15.
+//  Copyright © 2015 Kelly Alonso. All rights reserved.
+//
+
+import XCTest
+@testable import L6kalonso
+
+class L6kalonsoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
